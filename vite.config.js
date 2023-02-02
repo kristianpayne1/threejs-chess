57f9f27b1bb8f902c67a8ed659cf26eb63716226
Kristian Payne <kristianpayne1@gmail.com>
2023-02-02 21:28:16 +0000

    Add gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  bae: "/threejs-chess/",
+  base: "/threejs-chess/",
   plugins: [react()],
 });
